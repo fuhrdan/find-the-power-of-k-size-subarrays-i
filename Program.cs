5cdd57a005b9be3be73d55942d7d6bde9634e6a6
fuhrdan <165685619+fuhrdan@users.noreply.github.com>
2024-11-16 08:26:37 -0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿//*****************************************************************************
+//** Find the Power of K Size Subarrays I    leetcode                        **
+//*****************************************************************************
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* resultsArray(int* nums, int numsSize, int k, int* returnSize) {
+    *returnSize = numsSize - k + 1;
+    int* retArray = (int*)malloc((*returnSize) * sizeof(int));
+    int x = 0;
+    for(int i = 0; i <= numsSize - k; i++) 
+    {
+        bool isConsecutive = true;
+        for(int j = 1; j < k; j++) 
+        {
+            if(nums[i + j] != nums[i + j - 1] + 1) 
+            {
+                isConsecutive = false;
+                break;
+            }
+        }
+        
+        if(isConsecutive) 
+        {
+            retArray[x++] = nums[i + k - 1];
+        } 
+        else 
+        {
+            retArray[x++] = -1;
+        }
+    }
+    return retArray;
+}
